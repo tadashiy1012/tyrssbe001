532d4249f553d65ec0f87f7f1dc2bfba8b0f9b6b
Tadashi Yamazaki <yama3ki.tada4@gmail.com>
2019-11-14 22:11:55 +0900

    2nd commit

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-
 const portNum = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+const whiteList = ['http://localhost', 'https://tyrssbe001.herokuapp.com'];
+const corsOpts = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1) {
+	    callback(null, true);
+	} else {
+	    callback(new Error('Not allowed by CORS'));
+	}
+    }
+};
+
+app.get('/', cors(corsOpts), (req, res) => {
     res.send('hello!');
+});
+
+app.get('/hoge', (req, res) => {
+    res.send('hoge!');
 });
 
 app.listen(portNum, () => {
