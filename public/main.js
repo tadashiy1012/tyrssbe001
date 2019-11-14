5d1318cdcb6cb8647621d75c4c296c43bb6852b8
Tadashi Yamazaki <yama3ki.tada4@gmail.com>
2019-11-15 05:27:44 +0900

    6th commit

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const out = document.getElementById('out');
 const inUrl = document.getElementById('inUrl');
 document.getElementById('btn').addEventListener('click', () => {
     const url = inUrl.value;
-    fetch('http://localhost:3000/rss?url=' + url).then((resp) => {
+    fetch('/rss?url=' + url).then((resp) => {
         return resp.text();
     }).then((resp) => {
         out.innerHTML = resp;
